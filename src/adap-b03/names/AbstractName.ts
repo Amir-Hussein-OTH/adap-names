d21a13a68912ef3848616495ecc12ee05da41727
Miro <amir.hussein@fau.de>
2024-11-17 00:53:16 +0100

    feat(adap-b03): implement homework 3

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -1,57 +1,107 @@
-import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
-import { Name } from "./Name";
+import {Name, DEFAULT_DELIMITER, ESCAPE_CHARACTER} from "./Name";
+
 
 export abstract class AbstractName implements Name {
 
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter;
     }
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
-    }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        const unescapedComponents: string[] = [];
+        let i = 0;
+        while (i < this.getNoComponents()) {
+            unescapedComponents.push(this.unescape(this.getComponent(i), this.delimiter));
+            i++;
+        }
+        return unescapedComponents.join(delimiter);
     }
+
 
     public toString(): string {
         return this.asDataString();
     }
 
+
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const componentsArray: string[] = [];
+        let i = 0;
+        while (i < this.getNoComponents()) {
+            componentsArray.push(this.getComponent(i));
+            i++;
+        }
+        return componentsArray.join(this.delimiter);
+    }
+
+
+    public getHashCode(): number {
+        let hashCode: number = 0;
+        const s: string = this.toString() + this.getDelimiterCharacter();
+        for (let i = 0; i < s.length; i++) {
+            let c = s.charCodeAt(i);
+            hashCode = (hashCode << 5) - hashCode + c;
+            hashCode |= 0;
+        }
+        return hashCode;
+    }
+
+
+    public clone(): Name {
+        return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
     }
 
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        if (this.getNoComponents() !== other.getNoComponents()) {
+            return false;
+        }
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            if (this.getComponent(i) !== other.getComponent(i)) {
+                return false;
+            }
+        }
+        return this.getDelimiterCharacter() === other.getDelimiterCharacter();
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return !this.getNoComponents();
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
     abstract getNoComponents(): number;
 
     abstract getComponent(i: number): string;
+
     abstract setComponent(i: number, c: string): void;
 
     abstract insert(i: number, c: string): void;
+
     abstract append(c: string): void;
+
     abstract remove(i: number): void;
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        const otherDelimiter = other.getDelimiterCharacter();
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(this.escape(this.unescape(other.getComponent(i), otherDelimiter), this.delimiter));
+        }
     }
 
+
+    public escape(str: string, delimiter: string) {
+        return str.replaceAll(new RegExp(`(?<!\\${ESCAPE_CHARACTER})\\${delimiter}`, "g"), ESCAPE_CHARACTER + delimiter);
+    }
+
+    public unescape(str: string, delimiter: string) {
+        return str.replaceAll(new RegExp(`\\${ESCAPE_CHARACTER}\\${delimiter}`, "g"), delimiter);
+    }
+
+    public splitString(str: string, delimiter: string) {
+        return str.split(new RegExp(`(?<!\\${ESCAPE_CHARACTER})\\${delimiter}`));
+    }
 }
