11b3a32faa389d300fcf81ef74d4b23493236a4e
Miro <amir.hussein@fau.de>
2024-11-18 12:01:25 +0100

    Docs(adap-b03): add annotation to some methods

--- src/adap-b03/names/StringArrayName.ts
+++ src/adap-b03/names/StringArrayName.ts
@@ -1,11 +1,11 @@
 import {AbstractName} from "./AbstractName";
-import {Name} from "./Name";
 
 
 export class StringArrayName extends AbstractName {
 
     protected components: string[] = [];
 
+// @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
         super(delimiter);
         for (const item of other) {
@@ -13,27 +13,32 @@ export class StringArrayName extends AbstractName {
         }
     }
 
+    // @methodtype get-method
     public getNoComponents(): number {
         return this.components.length;
     }
 
-
+    // @methodtype get-method
     public getComponent(i: number): string {
         return this.escape(this.components[i], this.delimiter);
     }
 
+// @methodtype set-method
     public setComponent(i: number, c: string): void {
         this.components[i] = this.unescape(c, this.delimiter);
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         this.components.splice(i, 0, this.unescape(c, this.delimiter));
     }
 
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(this.unescape(c, this.delimiter));
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         this.components.splice(i, 1);
     }
--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -6,27 +6,31 @@ export class StringName extends AbstractName {
     protected name: string = "";
     protected length: number = 0;
 
+    // @methodtype initialization-method
     constructor(other: string, delimiter?: string) {
         super(delimiter);
         this.name = other;
         this.length = this.splitString(this.name, this.delimiter).length;
     }
 
+    // @methodtype get-method
     public getNoComponents(): number {
         return this.length;
     }
 
-
+    // @methodtype get-method
     public getComponent(i: number): string {
         return this.splitString(this.name, this.delimiter)[i];
     }
 
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         const array = this.splitString(this.name, this.delimiter);
         array[i] = c;
         this.name = array.join(this.delimiter);
     }
 
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         const array = this.splitString(this.name, this.delimiter);
         array.splice(i, 0, c);
@@ -34,17 +38,20 @@ export class StringName extends AbstractName {
         this.length += 1;
     }
 
+    // @methodtype command-method
     public append(c: string): void {
         this.name += this.delimiter + c;
         this.length += 1;
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         const array = this.splitString(this.name, this.delimiter);
         array.splice(i, 1);
         this.name = array.join(this.delimiter);
         this.length -= 1;
     }
+
     private splitString(str: string, delimiter: string) {
         return str.split(new RegExp(`(?<!\\${ESCAPE_CHARACTER})\\${delimiter}`));
     }
